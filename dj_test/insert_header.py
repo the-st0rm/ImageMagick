52289a8a1fd173a4751b3361ab97721a10abba3b
the_storm <eng.ibrahim@aucegypt.edu>
2016-06-07 10:50:04 +0100

    updating insert_header.py

--- dj_test/insert_header.py
+++ dj_test/insert_header.py
@@ -2,21 +2,30 @@
 
 import sys
 
-PREDATA_SIZE = 0x40
 
-fname = sys.argv[1]
-fd = open(fname,"rb")
-fdata = fd.read()
-fd.close()
 
-target_args = " convert"
 
-padding_size = PREDATA_SIZE-len(target_args)
+PREDATA_SIZE = 40
+MARKER = "-_-"
 
-predata = target_args.ljust(PREDATA_SIZE, "A")
+def main():
+	print "Main"
 
-fd2 = open(fname+".dj"+fname[fname.rfind('.'):],'wb')
-fd2.write(predata + fdata)
-fd2.close()
+	fname = sys.argv[1]
+	fd = open(fname,"rb")
+	fdata = fd.read()
+	fd.close()
 
-exit()
+	target_args = MARKER + "convert -trim"
+
+	padding_size = PREDATA_SIZE-len(target_args)
+
+	predata = target_args.ljust(PREDATA_SIZE, "\x00")
+
+	fd2 = open(fname+".dj"+fname[fname.rfind('.'):],'wb')
+	fd2.write(fdata+predata)
+	fd2.close()
+	return 
+
+if __name__ == '__main__':
+	main()
